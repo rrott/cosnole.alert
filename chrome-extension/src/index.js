1b56f2d8053508b2eb6858c6a0714574bbe995d7
Roman Rott <roman@rrott.com>
2021-07-25 00:27:40 +0300

    updated manifest. add lists tab. updatted footer

--- chrome-extension/src/index.js
+++ chrome-extension/src/index.js
@@ -1,15 +1,35 @@
-const embedScript = () => {
-  const isRemoveFromDom = false;
-  const scriptTag = document.createElement('script');
-  scriptTag.setAttribute("type", "module");
-  scriptTag.src = chrome.extension.getURL('src/consoleReplacer.js');
+// const tabId = getTabId();
 
-  if (isRemoveFromDom) {
-    scriptTag.addEventListener('load', () =>
-      this.parentNode.removeChild(this)
-    );
-  }
-  (document.head || document.documentElement).appendChild(scriptTag);
-}
 
-embedScript();
+chrome.action.onClicked.addListener(async (tab) => {
+  let userReq = await fetch('https://example.com/user-data.json');
+  let user = await userReq.json();
+  let givenName = user.givenName || '<GIVEN_NAME>';
+
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['src/consoleReplacer.js']
+    args: [givenName],
+  });
+});
+
+// chrome.scripting.executeScript(
+//   {
+//     target: {tabId: tabId, allFrames: true},
+//     files: ['src/consoleReplacer.js'],
+//   },
+//   () => {
+//     // const isRemoveFromDom = false;
+//     // const scriptTag = document.createElement('script');
+//     // scriptTag.setAttribute("type", "module");
+//     // scriptTag.src = chrome.extension.getURL('src/consoleReplacer.js');
+
+//     // if (isRemoveFromDom) {
+//     //   scriptTag.addEventListener('load', () =>
+//     //     this.parentNode.removeChild(this)
+//     //   );
+//     // }
+//     // (document.head || document.documentElement).appendChild(scriptTag);
+//   }
+// );
+
